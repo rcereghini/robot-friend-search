20991430be6ee3473c9affdef66faf6293b0fe7f
Robert Cereghini <rcereghini@gmail.com>
2018-07-17 21:49:46 -0700

    Complete project uploaded to repository. May undergo styling changes.

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Sparky Spunkertz',
+    username: 'SPunk',
+    email: 'spunkertz@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Bonkers Bingo',
+    username: 'BBingo',
+    email: 'bonkers@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Iz Zordok',
+    username: 'izZo',
+    email: 'zordok@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Bolt Bronzer',
+    username: 'BBronze',
+    email: 'Bolt@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Chip Zanuf',
+    username: 'Chipster',
+    email: 'cZanuf@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Gertrude Watts',
+    username: 'GWatts',
+    email: 'gwatts@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Pillar Bender',
+    username: 'PBender',
+    email: 'pBender@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'Keg Robo',
+    username: 'kRobo',
+    email: 'kegger@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'Plug In',
+    username: 'plugIn',
+    email: 'plug@gmail.com'
+  },
+  {
+    id: 10,
+    name: 'Hertz Roller',
+    username: 'hertzR',
+    email: 'hertz@gmail.com'
+  },
+]
